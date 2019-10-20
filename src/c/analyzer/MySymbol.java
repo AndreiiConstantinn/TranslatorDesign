bc23e557fac93682659a7e91db2f3d77b7140333
Florin Nica <florin1398@yahoo.com>
2019-10-20 13:59:54 +0300

    Added jflex as maven plugin and generated Lexer class and sym interface.

---
+++ src/c/analyzer/MySymbol.java
@@ -0,0 +1,34 @@
+package c.analyzer;
+
+import java_cup.runtime.Symbol;
+
+public class MySymbol extends Symbol {
+	private int line;
+	private int column;
+
+	public MySymbol(int type, int line, int column) {
+		super(type, -1, -1, null);
+	}
+
+	public MySymbol(int type, int line, int column, Object value) {
+		this(type, line, column, -1, -1, value);
+	}
+
+	public MySymbol(int type, int line, int column, int left, int right, Object value) {
+		super(type, left, right, value);
+		this.line = line;
+		this.column = column;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public String toString() {   
+		return "line: "+ line + ", column: "+ column;
+	}
+}
